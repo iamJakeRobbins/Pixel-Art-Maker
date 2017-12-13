02cbf0dbe3aa5256fde7c41c0294bfd14884b0be
Jake Robbins <jrobbinsd@gmail.com>
2017-12-13 09:34:49 -0700

    semantic naming

--- public/main.js
+++ public/main.js
@@ -3,30 +3,30 @@ var artArea = document.getElementsByClassName('artArea')[0];
 var color = "";
 var button = document.getElementsByTagName('button')[0];
 
+
 artArea.addEventListener('click', (event) => {
   event.preventDefault();
   if (event.target.className = 'grid' && color === 'blue') {
     event.target.className = 'blue'
-  }
-  else if (event.target.className = 'grid' && color === 'red') {
+  } else if (event.target.className = 'grid' && color === 'red') {
     event.target.className = 'red'
   }
 })
 
 palette.addEventListener('click', (event) => {
   event.preventDefault();
-  if (event.target.className === 'pblue') {
+  if (event.target.className === 'pallete_Blue') {
     color = 'blue';
-  }
-  else if (event.target.className === 'pred') {
+  } else if (event.target.className === 'pallete_Red') {
     color = 'red';
   }
 })
 
-// button.addEventListener('click', (event) => {
-//   event.preventDefault();
-//   var butterflies = artArea.children.className
-//   for (var i = 0; i < butterflies.length; i++) {
-//     butterflies[i]
-//   }
-// })
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  var gridReset = artArea.children
+  for (var i = 0; i < gridReset.length; i++) {
+    gridReset[i].className = 'grid'
+
+  }
+})
